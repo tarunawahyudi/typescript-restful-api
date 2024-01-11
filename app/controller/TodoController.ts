b5f19390eaf2002a184004f9bb0401537d29881a
Taruna Wahyudi <taruna.wahyudi@asetku.com>
2024-01-11 18:05:42 +0700

    update

--- app/controller/TodoController.ts
+++ app/controller/TodoController.ts
@@ -1,10 +1,9 @@
-import http, { RequestListener } from "http";
 import Controller from "../core/Controller";
 
 class TodoController extends Controller {
 
     public get() {
-        this._request;
+        this._response
     }
 }
 
--- app/index.ts
+++ app/index.ts
@@ -4,8 +4,13 @@ import TodoController from './controller/TodoController';
 
 
 const controller = new Controller();
+const todoController = new TodoController();
 
 http.createServer((request, response) => {
     controller.setRequest(request);
     controller.setResponse(response);
+
+    if (request.url === '/todo' && request.method === 'GET') {
+        todoController.get();
+    }
 }).listen(8080);
